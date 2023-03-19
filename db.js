1e98051f631901030e22fcf4d9f4c38c51fb525c
S Vijayaprasad <svijaym6@gmail.com>
2023-03-19 06:36:32 +0530

    db removed

--- db.js
+++
@@ -1,6 +0,0 @@
-// {
-//     "name":"mens shirt",
-//          "quantity":"6",
-//          "price":"2499",
-//          "imageurl":"https://user-images.githubusercontent.com/101625055/226092799-798f2e0b-7241-46a5-8315-2d8e0267473d.jpg"
-//    }
