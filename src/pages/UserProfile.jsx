cb41dcdd680a901fce1e81b77a5540b69bc262a0
S Vijayaprasad <svijaym6@gmail.com>
2023-04-12 21:24:18 +0530

    addeds

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -19,8 +19,9 @@ const UserProfile = () => {
     navigate("/");
   }
   console.log(data);
-   console.log(data);
-   console.log(data);
+  console.log(data);
+  console.log(data);
+  console.log(data);
   return (
     <div>
       <div>UserProfile</div>
