2da571f1cb59a24e11ec02c844a5595c8185d400
S Vijayaprasad <svijaym6@gmail.com>
2023-05-21 23:16:10 +0530

    hi

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -21,7 +21,8 @@ const UserProfile = () => {
   console.log(data);
   console.log(data);
   console.log(data);
-   console.log(data);
+  console.log(data);
+  console.log(data);
   return (
     <div>
       <div>UserProfile</div>
